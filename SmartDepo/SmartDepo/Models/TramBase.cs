8e6925b2949b820c4b899f16a52c1dd8a6490bf7
Valentin Horacek <valentin.horacek@gmail.com>
2025-03-29 10:07:14 +0100

    Comments, Clean-up

--- SmartDepo/SmartDepo/Models/TramBase.cs
+++ SmartDepo/SmartDepo/Models/TramBase.cs
@@ -6,20 +6,39 @@ namespace SmartDepo.Models
 {
     public class TramBase : ComponentBase
     {
+        /// <summary>
+        /// Injected TramService
+        /// </summary>
         [Inject]
         public ITramService TramService { get; set; }
 
+        /// <summary>
+        /// List of trams
+        /// </summary>
         public IEnumerable<Tram> Trams { get; set; }
 
+        /// <summary>
+        /// OnInitializedAsync - set Tram on initialization.
+        /// </summary>
+        /// <returns></returns>
         protected override async Task OnInitializedAsync()
         {
-            //Trams = (await TramService.GetDepoAsync()).ToList();
+            // Trams = (await TramService.GetDepoAsync()).ToList();
         }
+
+        /// <summary>
+        /// FetchData - fetches data from TramService.
+        /// </summary>
+        /// <returns></returns>
         public async Task FetchData()
         {
             Trams = await TramService.GetTramsAsync();
         }
 
+        /// <summary>
+        /// ScheduleNext - schedules next tram.
+        /// </summary>
+        /// <returns></returns>
         public async Task ScheduleNext()
         {
             Tram next = await TramService.GetNextAsync();
@@ -38,6 +57,11 @@ namespace SmartDepo.Models
             }
         }
 
+        /// <summary>
+        /// FreeTram - frees tram.
+        /// </summary>
+        /// <param name="tram">Tram to be freed from schedule.</param>
+        /// <returns></returns>
         public async Task FreeTram(Tram tram)
         {
             tram.HasSchedule = false;
@@ -45,6 +69,5 @@ namespace SmartDepo.Models
 
             Trams.Where(w => w.Id == tram.Id).First().HasSchedule = tram.HasSchedule;
         }
-
     }
 }
--- SmartDepo/SmartDepo/Services/TramService.cs
+++ SmartDepo/SmartDepo/Services/TramService.cs
@@ -1,28 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SmartDepo.Models;
-using System.Text;
+﻿using SmartDepo.Models;
 
 namespace SmartDepo.Services
 {
     public class TramService : ITramService
     {
+        /// <summary>
+        /// HttpClient
+        /// </summary>
         private readonly HttpClient httpClient;
 
+        /// <summary>
+        /// TramService constructor
+        /// </summary>
+        /// <param name="httpClient"></param>
         public TramService(HttpClient httpClient)
         {
             this.httpClient = httpClient;
         }
 
+        /// <summary>
+        /// FreeTramAsync - PUT call to API to update tram.
+        /// </summary>
+        /// <param name="tram"></param>
+        /// <returns></returns>
         public async Task<HttpResponseMessage> FreeTramAsync(Tram tram)
         {
             return await httpClient.PutAsJsonAsync($"api/Tram/{tram.Id}", tram);
         }
 
+        /// <summary>
+        /// GetNextAsync - GET call to API to get next tram, or create new.
+        /// </summary>
+        /// <returns></returns>
         public async Task<Tram> GetNextAsync()
         {
             return await httpClient.GetFromJsonAsync<Tram>("api/Tram/Next");
         }
 
+        /// <summary>
+        /// GetTramsAsync - GET call to API to get all trams.
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<Tram>> GetTramsAsync()
         {
             return await httpClient.GetFromJsonAsync<Tram[]>("api/Tram");
